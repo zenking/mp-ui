c05e1d9defe41b6ea23f4691f90183125a500bfd
zenking <zhangling@whatsmax.com>
2018-07-04 17:30:35 +0800

    upd pkg

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,6 +27,7 @@ module.exports = {
   },
   output: {
     path: config.build.assetsRoot,
+    libraryTarget: "umd",
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
